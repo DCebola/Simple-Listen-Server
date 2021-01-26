b1166dfbb490746d2939ac15fe90133567d926fc
diogo <d.cebola@campus.fct.unl.pt>
2021-01-26 15:06:13 +0000

    finished

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-    res.send(`Received request from${req.baseUrl}`)
-    res.send(
-        req.body
-    )
+    res.send(`Online`)
+})
+
+app.get('/sniff', (req, res) =>{
+    console.log(req)
+    res.send("Got it.")
 })
 
 app.listen(port, () => {
